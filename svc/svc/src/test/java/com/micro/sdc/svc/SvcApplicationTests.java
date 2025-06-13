69f193e5855f38d5541c0cc7eb8e46fbcd017e02
Chanakya-dev <Chanakyamanas123@gmail.com>
2025-06-13 06:28:52 +0530

    Added Jar Files

--- svc/svc/src/test/java/com/micro/sdc/svc/SvcApplicationTests.java
+++ svc/svc/src/test/java/com/micro/sdc/svc/SvcApplicationTests.java
@@ -2,12 +2,13 @@ package com.micro.sdc.svc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-class SvcApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+@TestPropertySource(properties = {
+    "eureka.client.enabled=false"
+})
+public class SvcApplicationTests {
+    // your tests
 }
+
