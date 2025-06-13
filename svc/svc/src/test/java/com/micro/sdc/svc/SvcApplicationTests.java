3ad9c3fa3f2539909100241e4e76fcc9744168e0
Chanakya-dev <Chanakyamanas123@gmail.com>
2025-06-13 06:44:34 +0530

    Revert "Added Jar Files"
    This reverts commit 69f193e5855f38d5541c0cc7eb8e46fbcd017e02.

--- svc/svc/src/test/java/com/micro/sdc/svc/SvcApplicationTests.java
+++ svc/svc/src/test/java/com/micro/sdc/svc/SvcApplicationTests.java
@@ -2,13 +2,12 @@ package com.micro.sdc.svc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-    "eureka.client.enabled=false"
-})
-public class SvcApplicationTests {
-    // your tests
-}
+class SvcApplicationTests {
 
+	@Test
+	void contextLoads() {
+	}
+
+}
